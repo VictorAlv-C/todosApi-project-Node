9780886bf93b80b57716dac0f7646a8edb8e9b26
Victor A. Alvarado <isc.cuvillas@gmail.com>
2022-03-03 11:13:29 -0600

    Environment variables added

--- api/utils/database.js
+++ api/utils/database.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const sequelize = new Sequelize({
-  host: process.env.DB_HOST, // localhost
-  username: process.env.DB_USER, // postgres
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: 5432,
-  database: process.env.DB, // example
+  database: process.env.DB,
   dialect: 'postgres'
 });
 
